fe9d87ca750f365ac5e03a12e7f68b4629272ba5
kirti-exe <kirti.exe@gmail.com>
2025-07-12 23:07:19 +0530

    Update SortArrOFf0s1s&2s.cpp

--- SortArrOFf0s1s&2s.cpp
+++ SortArrOFf0s1s&2s.cpp
@@ -1,33 +1,37 @@
-#include <iostream>
-#include <vector>
+#include<iostream>
+#include<vector>
 using namespace std;
-
-void sortColors(vector<int>& nums) {
-    int low = 0, mid = 0, high = nums.size() - 1;
-
-    while (mid <= high) {
-        if (nums[mid] == 0) {
-            swap(nums[low], nums[mid]);
-            low++;
-            mid++;
-        } else if (nums[mid] == 1) {
-            mid++;
-        } else { // nums[mid] == 2
-            swap(nums[mid], nums[high]);
-            high--;
+class Solution{
+public:
+    void sortArray(vector<int>& arr, int n){                            //time complexity: O(n)      space complexity: O(1)
+        int low = 0, mid = 0, high = n-1;
+        while(mid <= high){
+            if(arr[mid] == 0){
+                swap(arr[low], arr[mid]);
+                low++;
+                mid++;
+            } else if (arr[mid] == 1){
+                mid++;
+            } else {
+                swap(arr[mid], arr[high]);
+                high--;
+            }
         }
     }
-}
 
-int main() {
-    vector<int> nums = {2, 0, 2, 1, 1, 0};
-
-    sortColors(nums);
-
-    cout << "Sorted array: ";
-    for (int num : nums) {
-        cout << num << " ";
+    void printArray(vector<int>&arr, int n){
+        for(int i=0; i<n; i++){
+            cout<<arr[i]<<" ";
+        }cout<<endl;
     }
+};
+int main(){
+    vector<int> arr = {0,1,1,0,1,2,1,2,0,0,0};
+    int n = arr.size();
 
+    Solution sol;
+    sol.sortArray(arr, n);
+    cout<<"Sorted Array: ";
+    sol.printArray(arr, n);
     return 0;
 }
