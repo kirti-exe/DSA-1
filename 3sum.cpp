fa666593f8e5a77d86464975d5ce3135703525a4
kirti-exe <kirti.exe@gmail.com>
2025-07-14 12:36:41 +0530

    Update 3sum.cpp

--- 3sum.cpp
+++ 3sum.cpp
@@ -1,52 +1,47 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
-class Solution {
-public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> result;
-        sort(nums.begin(), nums.end()); // Sort array
+vector<vector<int>> triplet(int n, vector<int> &num){
+    vector<vector<int>> ans;
+    sort(num.begin(), num.end());
+    
+    for(int i = 0; i < n; i++){
+        if(i > 0 && num[i] == num[i - 1]) continue;
 
-        int n = nums.size();
-        for(int i = 0; i < n - 2; i++) {
-            if(i > 0 && nums[i] == nums[i - 1]) continue; // Skip duplicate i
-
-            int left = i + 1;
-            int right = n - 1;
-
-            while(left < right) {
-                int sum = nums[i] + nums[left] + nums[right];
-
-                if(sum == 0) {
-                    result.push_back({nums[i], nums[left], nums[right]});
-                    left++;
-                    right--;
-
-                    // Skip duplicates
-                    while(left < right && nums[left] == nums[left - 1]) left++;
-                    while(left < right && nums[right] == nums[right + 1]) right--;
-                }
-                else if(sum < 0) {
-                    left++;
-                }
-                else {
-                    right--;
-                }
+        int j = i + 1;
+        int k = n - 1;
+        
+        while(j < k){
+            int sum = num[i] + num[j] + num[k];
+            if(sum < 0){
+                j++;
+            }
+            else if(sum > 0){
+                k--;
+            }
+            else {
+                ans.push_back({num[i], num[j], num[k]});
+                j++;
+                k--;
+                // Skip duplicates
+                while(j < k && num[j] == num[j - 1]) j++;
+                while(j < k && num[k] == num[k + 1]) k--;
             }
         }
-        return result;
     }
-};
+    return ans;
+}
 
-int main() {
-    Solution sol;
-    vector<int> nums = {-1, 0, 1, 2, -1, -4};
-    vector<vector<int>> triplets = sol.threeSum(nums);
+int main(){
+    vector<int> arr = {-1, 0, 1, 2, -1, -4};
+    int n = arr.size();
+    
+    vector<vector<int>> result = triplet(n, arr);
 
-    for(auto triplet : triplets) {
-        for(int num : triplet) {
+    cout << "Unique triplets with sum 0:\n";
+    for(auto triplet : result){
+        for(auto num : triplet){
             cout << num << " ";
         }
         cout << endl;
