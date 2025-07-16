d1cd8df277942c84690069e49fd55088e5fff902
kirti-exe <kirti.exe@gmail.com>
2025-07-16 13:45:54 +0530

    Update 4Sum.cpp

--- 4Sum.cpp
+++ 4Sum.cpp
@@ -1,45 +1,63 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include<iostream>
+#include<vector>
+#include<algorithm>
 using namespace std;
-
+//Time Complexity: O(n^3), Space Complexity: no space used
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        vector<vector<int>> result;
         int n = nums.size();
-        if(n < 4) return result;
-
+        vector<vector<int>> ans;
         sort(nums.begin(), nums.end());
 
         for(int i = 0; i < n - 3; i++) {
-            // Avoid duplicates for the 1st number
-            if(i > 0 && nums[i] == nums[i - 1]) continue;
+            if(i > 0 && nums[i] == nums[i - 1]) continue;  // Skip duplicates for i
 
             for(int j = i + 1; j < n - 2; j++) {
-                // Avoid duplicates for the 2nd number
-                if(j > i + 1 && nums[j] == nums[j - 1]) continue;
+                if(j > i + 1 && nums[j] == nums[j - 1]) continue;  // Skip duplicates for j
 
-                int left = j + 1;
-                int right = n - 1;
+                int k = j + 1;
+                int l = n - 1;
 
-                while(left < right) {
-                    long long sum = (long long)nums[i] + nums[j] + nums[left] + nums[right];
+                while(k < l) {
+                    long long sum = (long long)nums[i] + nums[j] + nums[k] + nums[l];
 
                     if(sum == target) {
-                        result.push_back({nums[i], nums[j], nums[left], nums[right]});
+                        ans.push_back({nums[i], nums[j], nums[k], nums[l]});
+                        k++;
+                        l--;
 
-                        // Skip duplicates
-                        while(left < right && nums[left] == nums[left + 1]) left++;
-                        while(left < right && nums[right] == nums[right - 1]) right--;
-
-                        left++;
-                        right--;
-                    } else if(sum < target) {
-                        left++;
+                        while(k < l && nums[k] == nums[k - 1]) k++;  // Skip duplicates for k
+                        while(k < l && nums[l] == nums[l + 1]) l--;  // Skip duplicates for l
+                    }
+                    else if(sum < target) {
+                        k++;
                     } else {
-                        right--;
+                        l--;
                     }
                 }
             }
         }
+        return ans;
+    }
+};
+
+int main() {
+    vector<int> arr = {1, 0, -1, 0, -2, 2};
+    int target1 = 0;
+
+    vector<int> arr2 = {1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5};
+    int target2 = 8;
+
+    Solution s;
+
+    vector<vector<int>> result = s.fourSum(arr, target1);
+
+    for(const auto& quad : result) {
+        for(int num : quad)
+            cout << num << " ";
+        cout << endl;
+    }
+
+    return 0;
+}
