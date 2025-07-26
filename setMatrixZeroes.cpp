06e2a9f794a0ee89684486d1b5e7fe9546aeb6b0
kirti-exe <kirti.exe@gmail.com>
2025-07-27 00:39:13 +0530

    Create setMatrixZeroes.cpp

---
+++ setMatrixZeroes.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        bool firstRowZero = false, firstColZero = false;
+
+        // Check if first row has any zero
+        for (int j = 0; j < n; ++j) {
+            if (matrix[0][j] == 0) {
+                firstRowZero = true;
+                break;
+            }
+        }
+
+        // Check if first column has any zero
+        for (int i = 0; i < m; ++i) {
+            if (matrix[i][0] == 0) {
+                fir
