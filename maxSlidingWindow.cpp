d70dedb3a579af0cb7231842cba697d5e1b27bf7
kirti-exe <kirti.exe@gmail.com>
2025-08-11 22:48:43 +0530

    Create maxSlidingWindow.cpp

---
+++ maxSlidingWindow.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+#include<deque>
+using namespace std;
+
+vector<int> maxSlidingWindow(vector<int>& nums, int k){
+    deque<int> dq; //stores indices
+    vector<int> result;
+
+    for(int i=0; i<nums.size(); i++){
+        if(!dq.empty() && dq.front() <= i - k)
+            dq.pop_front();
+
+        while(!dq.empty() && nums[dq.back()] < nums[i])
+            dq.pop_back();
+
+        dq.push_back(i);
+
+        if(i >= k-1){
+            result.push_back(nums[dq.front()]);
+        }
+    }
+    return result;
+}
+int main()
+{
+    vector<int> nums = {1,3,-1,-3,5,3,6,7};
+    int k = 3;
+
+    vector<int> ans = maxSlidingWindow(nums, k);
+
+    for(int x:ans) {
+        cout<< x <<" ";
+    }cout<<endl;
+    return 0;
+}
