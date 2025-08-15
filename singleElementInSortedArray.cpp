09c5486b48a912c5f8db6b64cd0e5a2fb5ba7812
kirti-exe <kirti.exe@gmail.com>
2025-08-15 23:37:10 +0530

    Create singleElementInSortedArray.cpp

---
+++ singleElementInSortedArray.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int SingleNonDuplicate(vector<int>& nums){
+    int left = 0, right = nums.size() - 1;
+
+    while(left < right){
+        int mid = left + (right-left)/2;
+
+        if(mid % 2 == 1) mid--;
+
+        if(nums[mid] == nums[mid + 1]){
+            left = mid+2;
+        }
+
+        else {
+            right = mid;
+        }
+    }
+    return nums[left];
+}
+int main(){
+    vector<int> nums = {1,1,2,3,3,4,4,8,8};
+    cout<<SingleNonDuplicate(nums)<<endl;
+    return 0;
+}
